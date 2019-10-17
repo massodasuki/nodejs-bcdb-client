9397883d5f665dbac2de3018e57eed06ae9821c0
massodasuki <massodasuki@gmail.com>
2019-10-17 18:03:38 +0800

    - add get cpu from bcdb-machine - return as boolean if minCPU is achieve - install node-fetch

--- src/routes/cpu.js
+++ src/routes/cpu.js
@@ -1,45 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var http = require("http");
+var host = 'http://127.168.0.0:3000/systems/'
+
+const fetch = require('node-fetch');
+const minCpu = 10;
+var cpu = ''
+
+/* This will use by transactions */
 
 /* GET CPU from IoT Machine. */
-/* Get the IoT machine CPU using curl GET*/
+/*Send Boolean */
+
 router.get('/cpu/', function(req, res, next) {
 
-  http.get('http://nodejs.org/dist/index.json', (res) => {
-  const { statusCode } = res;
-  const contentType = res.headers['content-type'];
+  fetch(host)
+    .then(res => res.json())
+    .then(json => cpuJson = json);
 
-  let error;
-  if (statusCode !== 200) {
-    error = new Error('Request Failed.\n' +
-                      `Status Code: ${statusCode}`);
-  } else if (!/^application\/json/.test(contentType)) {
-    error = new Error('Invalid content-type.\n' +
-                      `Expected application/json but received ${contentType}`);
-  }
-  if (error) {
-    console.error(error.message);
-    // Consume response data to free up memory
-    res.resume();
-    return;
-  }
+    cpu = cpuJson.cpu;
+    console.log(cpu);
 
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
-    try {
-      const parsedData = JSON.parse(rawData);
-
-      console.log(parsedData);
-    } catch (e) {
-      console.error(e.message);
+    if (minCpu > cpu) {
+      res.send({'cpuStatus':true})
     }
-  });
-  }).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-  });
+
+    res.send({'cpuStatus':false})
 
 });
 
--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -6,63 +6,25 @@ var http = require("http");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-/* Retrive internal DATABASE and send to IoT Machine. */
-/* POST data into IoT machine CPU using POST curl*/
-/*If success delete database. */
-//Find
+/*This will be use by cron job*/
 
+/* Retrive internal DATABASE and send to IoT Machine. */
 router.get('/transactions/', function(req, res, next) {
+
+  //IF THE CPU FROM CPU JS IS TRUE http://127.168.0.0:5000/cpu/
 
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.collection("insurans").findOne({}, function(err, result) {
-      if (err) throw err;
-      console.log(result.name);
-      db.close();
+        dbo.collection("insurans").findOne({}, function(err, result) {
+          if (err) throw err;
+          console.log(result.name);
+          /* POST data into IoT machine CPU using POST curl*/
+          /*If success delete database. */
+          db.close(); });
     });
-  });
-
-  // const postData = querystring.stringify({
-  // 'msg': 'Hello World!'
-  // });
-
-  // const options = {
-  //   hostname: 'www.google.com',
-  //   port: 80,
-  //   path: '/upload',
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/x-www-form-urlencoded',
-  //     'Content-Length': Buffer.byteLength(postData)
-  //   }
-  // };
-  //
-  // const reqst = http.request(options, (res) => {
-  //   console.log(`STATUS: ${res.statusCode}`);
-  //   console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  //   res.setEncoding('utf8');
-  //   res.on('data', (chunk) => {
-  //     console.log(`BODY: ${chunk}`);
-  //   });
-  //   res.on('end', () => {
-  //     console.log('No more data in response.');
-  //   });
-  // });
-  //
-  // reqst.on('error', (e) => {
-  //   console.error(`problem with request: ${e.message}`);
-  // });
-  //
-  // // Write data to request body
-  // reqst.write(postData);
-  // reqst.end();
-
-
   res.send('respond with a resource');
+
 });
-
-
-
 
 module.exports = router;
